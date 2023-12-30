c33ccfea68b84579a7caf13cafb7ccb2a0ce4bf7
Aryan <132425739+Aryan-Thapa@users.noreply.github.com>
2023-12-30 13:45:58 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,48 +8,49 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int recsolve(int W, int wt[], int val[], int index)
+    
+    int solve(int capacity,int wt[],int val[],int index)
     {
         //basecase
-        if(index==0)
-        {
-            if(wt[0]<=W)return val[index];
+        if(index==0){
+            if(wt[0]<=capacity)return val[0];
             else return 0;
         }
         int include=0;
-        if(wt[index]<W)
+        if(wt[index]<=capacity)
         {
-            include=val[index]+recsolve(W-wt[index],wt,val,index-1);
+            include=val[index]+solve(capacity-wt[index],wt,val,index-1);
         }
-        int exclude=recsolve(W,wt,val,index-1);
+        int exclude=solve(capacity,wt,val,index-1);
         return max(include,exclude);
     }
     
-    int memsolve(int W, int wt[], int val[], int index,vector<vector<int>>&dp)
+    int solveusingmem(int capacity,int weight[], int value[], int index,vector<vector<int>>&dp)
     {
-        //basecase
         if(index==0)
         {
-            if(wt[index]<=W)return val[0];
+            if(weight[0]<=capacity)return value[0];
             else return 0;
+            
         }
-        if(dp[index][W]!=-1)return dp[index][W];
+        if(dp[index][capacity]!=-1)return dp[index][capacity];
         int include=0;
-        if(wt[index]<=W)
+        if(weight[index]<=capacity)
         {
-            include=val[index]+memsolve(W-wt[index],wt,val,index-1,dp);
+            include=value[index]+solveusingmem(capacity-weight[index],weight,value,index-1,dp);
+            
         }
-        int exclude=memsolve(W,wt,val,index-1,dp);
-        dp[index][W]=max(include,exclude);
-        return dp[index][W];
-    }    
+        int exclude=solveusingmem(capacity,weight,value,index-1,dp);
+        dp[index][capacity]=max(include,exclude);
+        return dp[index][capacity];
+    }
+    
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       //int ans=recsolve(W,wt,val,n-1);
-       vector<vector<int>>dp(n,vector<int>(W+1,-1));
-       int ans=memsolve(W,wt,val,n-1,dp);
-       return ans;
+       vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
+       return solveusingmem(W,wt,val,n-1,dp);
+    //   return solve(W,wt,val,n-1);
     }
 };
 
